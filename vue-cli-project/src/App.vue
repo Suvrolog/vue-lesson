3d4a724c7f925b728b832702b741bc585aca0647
IRBIS <648429389w@gmail.com>
2022-08-05 23:11:23 +0300

    выполнил задание №3

---
+++ vue-cli-project/src/App.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="sample">
+      <h2 v-show="todoItems.length == 0">Please add new task</h2>
+      <h2 v-show="todoItems.length != 0"> {{ message }} </h2>
+      
+      
+
+      <form class="input-group  mb-3" action="" v-on:submit.prevent="filterAll">
+        <div class="input-group mb-3" >
+          <input type="text" class="form-control" v-model.trim="todoItems.text" placeholder="Add new Task">
+          <button v-bind:class="buttonSwitch" v-on:click="addTask"  class="btn" >Add</button> 
+        </div>
+          
+      </form>
+
+ <ul class="list-group mb-3 d-flex column">
+        <li class="list-group-item " >
+            Completed tasks {{ sum() }}  
+        </li>
+        <li class="list-group-item"  >
+            All tasks {{ todoItems.length }}
+        </li>
+        
+      </ul>
+        
+      <div>
+        <app-progress-bar class="mb-3" v-bind:max="todoItems.length" v-bind:complete="sum()"  ></app-progress-bar>
+      </div>
+
+      <div class="btn-group mb-3" role="group" aria-label="Basic radio toggle button group">
+        <input type="radio" class="btn-check" name="btnradio" id="btnradio1" v-on:click="filterAll" autocomplete="off" checked>
+        <label class="btn btn-outline-primary" for="btnradio1">All Task</label>
+
+        <input type="radio" class="btn-check" name="btnradio" id="btnradio2" v-on:click="filterTrue" autocomplete="off">
+        <label class="btn btn-outline-primary" for="btnradio2">Comleted Task</label>
+
+        <input type="radio" class="btn-check" name="btnradio" id="btnradio3" v-on:click="filterFalse" autocomplete="off">
+        <label class="btn btn-outline-primary" for="btnradio3">Uncompleted Task</label>
+      </div>
+
+      
+      
+      <form class="input-group mb-3" action="" v-on:submit.prevent="filterTask">
+        <div class="input-group mb-3" >
+        <input placeholder="Search" class="form-control" type="text" v-model.trim="search" />
+        <button class="btn btn-primary" >Search</button> 
+        </div>
+      </form>
+
+      
+            
+      <ul class="list-group">
+        <li class="list-group-item" v-for="item in arrayClone" :key="item.id">
+            <div class="mb-3">
+            <button v-bind:class="{ 'btn-success':item.done == true, 'btn-secondary':item.done == false }" v-on:click="item.done = !item.done" class="btn" >{{ item.done ? "Completed" : "In order" }}</button>  
+            <span v-on:click="isActive=!isActive" class="mouse-point"> {{ item.text }} </span> 
+            <button class="btn btn-primary" v-on:click="s">-</button>
+            </div>
+            <form class="input-group" v-show="isActive" action="" v-on:submit.prevent="">
+            <div class="input-group" >
+            <input placeholder="Add Task" v-model="item.text" class="form-control" type="text"/>
+            <button class="btn btn-primary" v-on:click="isActive=!isActive">Accept</button> 
+            </div>
+          </form>
+
+
+        </li>
+      </ul>
+      
+    </div>
+</template>
+
+<script>
+import AppProgressBar from './components/AppProgressBar.vue'
+export default {
+  name: 'App',
+  components: {
+    'app-progress-bar':AppProgressBar
+  },
+   data() {
+          return {
+            'btn-basic': false,
+            'btn-primary': true,
+            search: '',
+            i: 0,
+            message: 'Hello from Vue App',
+            isActive: false,
+            todoItems: [],
+            arrayClone: [],
+          }
+        },
+
+        methods: {
+          doneTask (){
+            this.arrayClone =  this.todoItems.filter((item) => item.done == true);
+            return this.arrayClone.length;
+          },
+
+
+          delItem(){
+            console.log(this.item.text)
+          },
+
+          addTask(){
+            if(this.todoItems.text !== undefined && this.todoItems.text.length !== 0) {
+              this.todoItems.push({id: this.i, text: this.todoItems.text, done: false});
+              this.i++;
+              this.todoItems.text = undefined;
+            } else {
+              alert("Вы не ввели текст задачи");
+            }
+          },
+
+          filterFalse(){
+            this.arrayClone = this.todoItems.filter((item) => !item.done);
+          },
+
+          filterTrue(){
+            this.arrayClone =  this.todoItems.filter((item) => item.done);
+          },
+
+          filterAll(){
+            this.arrayClone = this.todoItems;
+          },
+
+          filterTask(){
+            this.arrayClone = this.arrayClone.filter(item => {
+              return item.text.toLowerCase().includes(this.search.toLowerCase())
+            });
+          },
+          sum(){
+            let sum = this.todoItems.filter((item) => item.done == true);
+            return sum.length;
+          },
+        
+        },
+
+        computed: {
+
+          
+          buttonSwitch() {
+            return {
+              'btn-secondary': this.todoItems.text == undefined,
+              'btn-primary': this.todoItems.text != undefined,
+            }
+          },
+
+        
+         
+        },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 60%;
+  color: #2c3e50;
+  margin: 60px auto 20px auto;
+}
+.displaynone{
+  display: none;
+}
+.mouse-point{
+  cursor: pointer;
+}
+
+</style>
---
+++ vue-cli-project/src/components/AppProgressBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="progress">
+        <div class="progress-bar" v-bind:style="progressCalc"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+          max: Number,
+          complete: Number,
+        },
+  computed: {
+    progressCalc(){
+      let width = this.complete / this.max * 100;
+        return {
+          width: width + '%'
+        };
+    }
+    
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
