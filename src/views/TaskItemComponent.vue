e35c638a689a4f0e2059e950a3d57a2c0651fc7c
IRBIS <648429389w@gmail.com>
2022-08-16 00:55:36 +0300

    выполнил задание №5 Задачи 1-3

---
+++ src/views/TaskItemComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>Task by Id {{id}} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$router.currentRoute.value.params.id
+    }
+  },
+}
+</script>
